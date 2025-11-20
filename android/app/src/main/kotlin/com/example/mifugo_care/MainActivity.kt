e2f5b57e2ea695f786b4f10074b2e1c77e9866c4
User <user@example.com>
2025-11-20 17:58:23 +0300

    Delete all files

--- android/app/src/main/kotlin/com/example/mifugo_care/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mifugo_care
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/services/ml_service.dart
+++
@@ -1,2 +0,0 @@
-// Conditional export: Use web implementation on web, mobile implementation on native platforms
-export 'ml_service_web.dart' if (dart.library.io) 'ml_service_mobile.dart';
